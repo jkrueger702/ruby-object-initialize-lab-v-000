c7f657e8577558fbe6eb8099ee7f9fe4d67e0abb
Jordan Krueger <jkrueger702@gmail.com>
2018-06-25 01:00:33 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,17 @@
 class Dog
+
+  def initialize(name)
+    @name = name
+  end
+
+  def name=(breed)
+    @name = name
+  end
+
+  def breed
+    @breed
+  end
+
   def initialize(breed="Mutt")
     @breed = breed
   end
